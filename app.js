d19bb583e4c8131a7c0be966e2115336069ab53e
Rob Davis <bfe5e925@opayq.com>
2022-11-29 22:12:47 +1000

    Finished reactivity

--- app.js
+++ app.js
@@ -11,37 +11,51 @@ const insertAfter = (newNode, existingNode) => {
 }
 
 const getData = () => {
-    let data = getCookie('data')
+    let data = window.localStorage.getItem('data')
 
     if (!data) {
         data = prompt('Enter Json Data')
-        setCookie('data', data, 180)
+        window.localStorage.setItem('data', data)
     }
 
     fill(data)
 }
 
-const getCookie = (cname) => {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+const formatDate = (date) => {
+    var day = ("0" + date.getDate()).slice(-2)
+    var month = ("0" + (date.getMonth() + 1)).slice(-2)
+    return date.getFullYear()+"-"+(month)+"-"+(day)
 }
 
-const setCookie = (cname, cvalue, exdays) => {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+const updateRowCost = (row) => {
+    const count = Number.parseInt(row.getElementsByClassName("item-number")[0].value)
+    const price = Number.parseFloat(row.getElementsByClassName("item-price")[0].value)
+    row.getElementsByClassName("item-total")[0].innerHTML = count * price
+    calculateTotal()
+}
+
+const hookupRowEvents = (row) => {
+    row.getElementsByClassName("item-number")[0].addEventListener('input', () => updateRowCost(row))
+    row.getElementsByClassName("item-price")[0].addEventListener('input', () => updateRowCost(row))
+}
+
+const addZeroes = (num) => {
+    return num.toLocaleString("en", {useGrouping: false, minimumFractionDigits: 2})
+}
+
+const calculateTotal = () => {
+    const totals = document.getElementsByClassName("item-total")
+
+    let total = 0
+
+    Array.prototype.forEach.call(totals, (t) => {
+        total += Number.parseFloat(t.innerHTML)
+    })
+
+    const placeholders = document.getElementsByClassName("final-total")
+    Array.prototype.forEach.call(placeholders, (ph) => {
+        ph.innerHTML = "$" + addZeroes(total)
+    })
 }
 
 const setup = () => {
@@ -50,11 +64,20 @@ const setup = () => {
         const lastRow = table.rows[table.rows.length - 1]
         const newRow = lastRow.cloneNode(true)
         insertAfter(newRow, lastRow)
+        hookupRowEvents(newRow)
     })
 
     getData()
 
-    // Hook up date and value calculation handlers
+    document.getElementsByClassName("issuedate")[0].addEventListener('input', (e) => {
+        const date = new Date(e.target.value)
+        date.setMonth(date.getMonth() + 1)
+        document.getElementsByClassName("duedate")[0].value = formatDate(date)
+    })
+
+    const rows = document.getElementsByClassName("data-row")
+
+    Array.prototype.forEach.call(rows, (row) => hookupRowEvents(row))
 }
 
 setup()
